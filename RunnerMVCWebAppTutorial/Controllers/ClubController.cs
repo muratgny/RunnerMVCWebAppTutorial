7874476bf68c384e90a564071d90da6675dae5f8
Murat Gunay <42067486+muratgny@users.noreply.github.com>
2023-06-15 16:44:38 +0200

    Conroller and Views are prepared for basic work

---
+++ RunnerMVCWebAppTutorial/Controllers/ClubController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RunnerMVCWebAppTutorial.Data;
+
+namespace RunnerMVCWebAppTutorial.Controllers
+{
+    public class ClubController : Controller
+    {
+        AppDbContext _context;
+        public ClubController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()//this "index" name must be also view's name under views/club folder. So it is seen directly
+        {
+            var clubs = _context.Clubs.ToList();
+            return View(clubs);
+        }
+    }
+}
---
+++ RunnerMVCWebAppTutorial/Controllers/RaceController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RunnerMVCWebAppTutorial.Data;
+
+namespace RunnerMVCWebAppTutorial.Controllers
+{
+    public class RaceController : Controller
+    {
+        AppDbContext _context;
+        public RaceController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var races = _context.Races.ToList();
+            return View(races);
+        }
+    }
+}
